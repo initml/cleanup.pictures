5b7284236b735e9962c1ea20380d473ed0d16b29
Cyril Diagne <diagne.cyril@gmail.com>
2021-12-10 15:26:22 +0100

    fix layout when no javascript

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,16 @@ import App from './App'
 import FirebaseProvider from './adapters/firebase'
 import UserProvider from './adapters/user'
 
+const root = document.createElement('div')
+root.id = 'root'
+root.classList.add('h-full')
+document.body.prepend(root)
+
 ReactDOM.render(
   <FirebaseProvider>
     <UserProvider>
       <App />
     </UserProvider>
   </FirebaseProvider>,
-  document.getElementById('root')
+  root
 )
