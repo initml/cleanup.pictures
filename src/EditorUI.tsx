3e909b03010674f414ea541386b1bd1dc24cd67c
Cyril Diagne <diagne.cyril@gmail.com>
2022-01-18 14:25:22 +0100

    vertically center action bar

--- src/EditorUI.tsx
+++ src/EditorUI.tsx
@@ -198,141 +198,156 @@ export default function EditorUI({
 
   return (
     <>
-      <div
+      <main
         className={[
-          isInpaintingLoading
-            ? 'animate-pulse-fast pointer-events-none transition-opacity'
-            : '',
+          'flex flex-1 flex-col sm:items-center sm:justify-center overflow-hidden',
+          'h-full',
+          'items-center justify-center',
         ].join(' ')}
+      >
+        <div
+          className={[
+            isInpaintingLoading
+              ? 'animate-pulse-fast pointer-events-none transition-opacity'
+              : '',
+          ].join(' ')}
+          style={{
+            height: scale !== 1 ? image.naturalHeight * scale : undefined,
+          }}
+        >
+          <div
+            className={[scale !== 1 ? '' : 'relative'].join(' ')}
+            style={{
+              transform: `scale(${scale})`,
+              transformOrigin: 'top center',
+            }}
+          >
+            <canvas
+              className="rounded-sm"
+              style={showBrush ? { cursor: 'none' } : {}}
+              ref={r => {
+                // console.log('ref', context)
+                if (r && !context) {
+                  const ctx = r.getContext('2d')
+                  if (ctx) {
+                    setContext(ctx)
+                  }
+                }
+              }}
+            />
+            <div
+              className={[
+                'absolute top-0 right-0 pointer-events-none',
+                'overflow-hidden',
+                'border-primary',
+                showSeparator ? 'border-l-4' : '',
+              ].join(' ')}
+              style={{
+                width: showOriginal
+                  ? `${Math.round(image.naturalWidth)}px`
+                  : '0px',
+                height: image.naturalHeight,
+                transitionProperty: 'width, height',
+                transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+                transitionDuration: '300ms',
+              }}
+            >
+              <img
+                className="absolute right-0"
+                src={image.src}
+                alt="original"
+                width={`${image.naturalWidth}px`}
+                height={`${image.naturalHeight}px`}
+                style={{
+                  width: `${image.naturalWidth}px`,
+                  height: `${image.naturalHeight}px`,
+                  maxWidth: 'none',
+                }}
+              />
+            </div>
+          </div>
+
+          {showBrush && (
+            <div
+              className="hidden sm:block absolute rounded-full border border-primary bg-primary bg-opacity-80 pointer-events-none"
+              style={{
+                width: `${brushSize * scale}px`,
+                height: `${brushSize * scale}px`,
+                left: `${x}px`,
+                top: `${y}px`,
+                transform: 'translate(-50%, -50%)',
+              }}
+            />
+          )}
+        </div>
+      </main>
+
+      <div
+        className="fixed w-full flex justify-center items-center bottom-0 bg-red"
         style={{
-          height: scale !== 1 ? image.naturalHeight * scale : undefined,
+          // Center the action bar in the white area available.
+          height: `${(window.innerHeight - image.naturalHeight * scale) / 2}px`,
         }}
       >
         <div
-          className={[scale !== 1 ? '' : 'relative'].join(' ')}
-          style={{
-            transform: `scale(${scale})`,
-            transformOrigin: 'top center',
-          }}
+          className={[
+            'flex items-center space-x-8 max-w-3xl',
+            'bg-gray-200 bg-opacity-50 backdrop-blur px-4 rounded-2xl',
+            'p-2 sm:px-6 sm:py-2',
+            'justify-evenly',
+          ].join(' ')}
         >
-          <canvas
-            className="rounded-sm"
-            style={showBrush ? { cursor: 'none' } : {}}
-            ref={r => {
-              // console.log('ref', context)
-              if (r && !context) {
-                const ctx = r.getContext('2d')
-                if (ctx) {
-                  setContext(ctx)
-                }
+          {edits.length > 1 || currentEdit.lines.length > 1 ? (
+            <Button
+              icon={
+                <svg
+                  width="19"
+                  height="9"
+                  viewBox="0 0 19 9"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                >
+                  <path
+                    d="M2 1C2 0.447715 1.55228 0 1 0C0.447715 0 0 0.447715 0 1H2ZM1 8H0V9H1V8ZM8 9C8.55228 9 9 8.55229 9 8C9 7.44771 8.55228 7 8 7V9ZM16.5963 7.42809C16.8327 7.92721 17.429 8.14016 17.9281 7.90374C18.4272 7.66731 18.6402 7.07103 18.4037 6.57191L16.5963 7.42809ZM16.9468 5.83205L17.8505 5.40396L16.9468 5.83205ZM0 1V8H2V1H0ZM1 9H8V7H1V9ZM1.66896 8.74329L6.66896 4.24329L5.33104 2.75671L0.331035 7.25671L1.66896 8.74329ZM16.043 6.26014L16.5963 7.42809L18.4037 6.57191L17.8505 5.40396L16.043 6.26014ZM6.65079 4.25926C9.67554 1.66661 14.3376 2.65979 16.043 6.26014L17.8505 5.40396C15.5805 0.61182 9.37523 -0.710131 5.34921 2.74074L6.65079 4.25926Z"
+                    fill="currentColor"
+                  />
+                </svg>
               }
-            }}
-          />
-          <div
-            className={[
-              'absolute top-0 right-0 pointer-events-none',
-              'overflow-hidden',
-              'border-primary',
-              showSeparator ? 'border-l-4' : '',
-            ].join(' ')}
-            style={{
-              width: showOriginal
-                ? `${Math.round(image.naturalWidth)}px`
-                : '0px',
-              height: image.naturalHeight,
-              transitionProperty: 'width, height',
-              transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
-              transitionDuration: '300ms',
-            }}
-          >
-            <img
-              className="absolute right-0"
-              src={image.src}
-              alt="original"
-              width={`${image.naturalWidth}px`}
-              height={`${image.naturalHeight}px`}
-              style={{
-                width: `${image.naturalWidth}px`,
-                height: `${image.naturalHeight}px`,
-                maxWidth: 'none',
-              }}
+              onClick={undo}
+            />
+          ) : (
+            <></>
+          )}
+          <div className="py-2">
+            <Slider
+              label={
+                <span>
+                  <span className="hidden sm:inline">Brush </span>Size
+                </span>
+              }
+              min={10}
+              max={150}
+              value={brushSize}
+              onChange={setBrushSize}
             />
           </div>
+          {editor.useHD ? (
+            <Button
+              primary
+              disabled={isInpaintingLoading || currentEdit.lines.length <= 1}
+              onClick={async () => {
+                setIsInpaintingLoading(true)
+                await render()
+                setIsInpaintingLoading(false)
+              }}
+            >
+              Clean HD
+            </Button>
+          ) : (
+            <></>
+          )}
         </div>
-
-        {showBrush && (
-          <div
-            className="hidden sm:block absolute rounded-full border border-primary bg-primary bg-opacity-80 pointer-events-none"
-            style={{
-              width: `${brushSize * scale}px`,
-              height: `${brushSize * scale}px`,
-              left: `${x}px`,
-              top: `${y}px`,
-              transform: 'translate(-50%, -50%)',
-            }}
-          />
-        )}
-      </div>
-
-      <div
-        className={[
-          'absolute bottom-3',
-          'flex items-center space-x-8 max-w-3xl',
-          'bg-gray-200 bg-opacity-50 backdrop-blur px-4 rounded-2xl',
-          'p-2 sm:px-6 sm:py-2',
-          'fixed justify-evenly',
-        ].join(' ')}
-      >
-        {edits.length > 1 || currentEdit.lines.length > 1 ? (
-          <Button
-            icon={
-              <svg
-                width="19"
-                height="9"
-                viewBox="0 0 19 9"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-              >
-                <path
-                  d="M2 1C2 0.447715 1.55228 0 1 0C0.447715 0 0 0.447715 0 1H2ZM1 8H0V9H1V8ZM8 9C8.55228 9 9 8.55229 9 8C9 7.44771 8.55228 7 8 7V9ZM16.5963 7.42809C16.8327 7.92721 17.429 8.14016 17.9281 7.90374C18.4272 7.66731 18.6402 7.07103 18.4037 6.57191L16.5963 7.42809ZM16.9468 5.83205L17.8505 5.40396L16.9468 5.83205ZM0 1V8H2V1H0ZM1 9H8V7H1V9ZM1.66896 8.74329L6.66896 4.24329L5.33104 2.75671L0.331035 7.25671L1.66896 8.74329ZM16.043 6.26014L16.5963 7.42809L18.4037 6.57191L17.8505 5.40396L16.043 6.26014ZM6.65079 4.25926C9.67554 1.66661 14.3376 2.65979 16.043 6.26014L17.8505 5.40396C15.5805 0.61182 9.37523 -0.710131 5.34921 2.74074L6.65079 4.25926Z"
-                  fill="currentColor"
-                />
-              </svg>
-            }
-            onClick={undo}
-          />
-        ) : (
-          <></>
-        )}
-        <div className="py-2">
-          <Slider
-            label={
-              <span>
-                <span className="hidden sm:inline">Brush </span>Size
-              </span>
-            }
-            min={10}
-            max={150}
-            value={brushSize}
-            onChange={setBrushSize}
-          />
-        </div>
-        {editor.useHD ? (
-          <Button
-            primary
-            disabled={isInpaintingLoading || currentEdit.lines.length <= 1}
-            onClick={async () => {
-              setIsInpaintingLoading(true)
-              await render()
-              setIsInpaintingLoading(false)
-            }}
-          >
-            Clean HD
-          </Button>
-        ) : (
-          <></>
-        )}
       </div>
     </>
   )
