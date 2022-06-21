eccb9c7fdedd7b6fc69cf25ebea47898fe2c15fb
Cyril Diagne <cyril@initml.co>
2022-06-21 11:22:53 +0200

    use internal endpoint

--- src/adapters/inpainting.ts
+++ src/adapters/inpainting.ts
@@ -24,8 +24,8 @@ export default async function inpaint(
   fd.append('refiner', refiner)
   fd.append('hd', isHD ? 'true' : 'false')
 
-  if (!process.env.REACT_APP_INPAINTING_ENDPOINT) {
-    throw new Error('missing env var REACT_APP_INPAINTING_ENDPOINT')
+  if (!process.env.REACT_APP_INPAINTING_ENDPOINT_INTERNAL) {
+    throw new Error('missing env var REACT_APP_INPAINTING_ENDPOINT_INTERNAL')
   }
   const headers: Record<string, any> = {}
   // Add the app check token.
@@ -43,7 +43,7 @@ export default async function inpaint(
   headers['X-REFINER'] = refiner
 
   // Make the request.
-  const res = await fetch(process.env.REACT_APP_INPAINTING_ENDPOINT, {
+  const res = await fetch(process.env.REACT_APP_INPAINTING_ENDPOINT_INTERNAL, {
     method: 'POST',
     headers,
     body: fd,
